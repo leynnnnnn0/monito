d3d271e15a8210632fd8fd930dd2e90a57ee2f9e
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-09-29 10:55:35 +0800

    finised mobile view of homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,6 +46,27 @@
         </div>
     </section>
 
-    
+    <section class="p-5">
+        <div>
+            <span class="font-medium">You already know?</span>
+            <h1 class="text-text-primary font-semibold text-lg">Useful Pet Knowledge</h1>
+        </div>
+        <x-knowledge-container />
+        <x-knowledge-container />
+        <x-knowledge-container />
+    </section>
+
+    <footer class="flex flex-col gap-4 items-center bg-primary-gradient p-5">
+        <img class="h-[30px]" src="{{ Vite::asset('resources/images/logo.svg')}}" alt="logo">
+        <div class="flex items-center justify-center gap-2">
+            <span class="text-gray-400 text-[10px]">Terms of Service</span>
+            <span class="text-gray-400 text-[10px]">Privacy Policy</span>
+        </div>
+        <span class="text-gray-400 text-[10px]">
+            2024 Monito. All rights reservered.
+        </span>
+    </footer>
+
+
 
 </x-layouts.main>
