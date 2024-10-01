07d5bab551a8ae3578b936880063f2912c60c8eb
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-10-01 20:24:59 +0800

    Include storage/app/public in the repository

--- app/Livewire/Cart.php
+++ app/Livewire/Cart.php
@@ -6,6 +6,15 @@ use Livewire\Component;
 
 class Cart extends Component
 {
+    public $items;
+
+    public function mount()
+    {
+        if (session()->has('cart_items')) {
+            $this->items = session()->get('cart_items');
+        }
+    }
+
     public function render()
     {
         return view('livewire.cart');
