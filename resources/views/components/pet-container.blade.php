ede4cd6ed708f66b799f82a7e1688645d7cbb9cb
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-10-01 15:03:04 +0800

    fixed the size issue of the image on pet-container

--- resources/views/components/pet-container.blade.php
+++ resources/views/components/pet-container.blade.php
@@ -1,6 +1,6 @@
 @props(['breed' => 'Pomeranian White', 'gender' => 'Male', 'price' => '90400', 'source' => 'test', 'id' => null])
-<a href="/category/show/{{ $id }}" class="cursor-pointer bg white w-auto h-54 p-2 shadow-sm space-y-3 rounded-lg" wire:navigate>
-    <img class="h-32" src="{{ asset('storage/' . $source)}}" alt="pet">
+<a href="/category/show/{{ $id }}" class="cursor-pointer bg white w-auto h-54 p-2 shadow-lg space-y-3 rounded-lg" wire:navigate>
+    <img class="h-32 w-full" src="{{ asset('storage/' . $source)}}" alt="pet">
     <div class="flex flex-col">
         <h1 class="text-sm font-semibold">{{ $breed}}</h1>
         <span class="text-gray-400 text-xs">Gender: <span class="text-gray-600 text-xs">{{ $gender}}</span></span>
