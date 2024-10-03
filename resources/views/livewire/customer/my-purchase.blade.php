8e0d735e7e5f103a16887492dac2a4b25126f2e0
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-10-04 06:31:58 +0800

    finished the initial order container for my purchase page

--- resources/views/livewire/customer/my-purchase.blade.php
+++ resources/views/livewire/customer/my-purchase.blade.php
@@ -19,4 +19,57 @@
             </x-tab-option>
         </div>
     </div>
+    <!-- Items -->
+    <div class="mx-auto max-w-[1000px] space-y-3">
+        <div class="flex flex-col gap-2 rounded-lg shadow-lg p-3 h-fit">
+            <section class="flex items-center justify-between border-b border-gray-300 h-fit ">
+                <x-logo class="size-16" />
+                <span class="text-green-500">Order Delivered</span>
+            </section>
+
+            <section class="flex items-center justify-between border-b border-gray-300">
+                <div class="flex items-center gap-2">
+                    <img class="sm:size-16 size-8" src="{{ Vite::asset('resources/images/shiba.png')}}" alt="dog image">
+                    <section class="flex flex-col py-4">
+                        <span class="text-text-primary text-lg font-bold">Shiba Inu </span>
+                        <span class="text-gray-500 text-xs font-light">Color: Yellow & White</span>
+                        <span class="text-gray-500 text-xs font-light">Size: small</span>
+                        <span class="text-xs font-light">x1</span>
+                    </section>
+                </div>
+                <span class="text-text-primary font-bold">$7,800.00</span>
+            </section>
+
+            <section class="flex items-center justify-end py-2">
+                <span class="text-gray-900 font-light">Order Total: <span class="text-text-primary text-lg font-bold">$7,800.00</span></span>
+            </section>
+        </div>
+        <div class="flex flex-col gap-2 rounded-lg shadow-lg p-3 h-fit">
+            <section class="flex items-center justify-between border-b border-gray-300 h-fit ">
+                <x-logo class="size-16" />
+                <span class="text-green-500">Order Delivered</span>
+            </section>
+
+            <section class="flex items-center justify-between border-b border-gray-300">
+                <div class="flex items-center gap-2">
+                    <img class="sm:size-16 size-8" src="{{ Vite::asset('resources/images/shiba.png')}}" alt="dog image">
+                    <section class="flex flex-col py-4">
+                        <span class="text-text-primary text-lg font-bold">Shiba Inu </span>
+                        <span class="text-gray-500 text-xs font-light">Color: Yellow & White</span>
+                        <span class="text-gray-500 text-xs font-light">Size: small</span>
+                        <span class="text-xs font-light">x1</span>
+                    </section>
+                </div>
+                <span class="text-text-primary font-bold">$7,800.00</span>
+            </section>
+
+            <section class="flex items-center justify-end py-2">
+                <span class="text-gray-900 font-light">Order Total: <span class="text-text-primary text-lg font-bold">$7,800.00</span></span>
+            </section>
+        </div>
+    </div>
+
+
+
+
 </div>
